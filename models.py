ed820353b04f89245494ee49fe20aa4db5254dc4
WeronikaSzadzinska <65089636+WeronikaSzadzinska@users.noreply.github.com>
2020-09-09 12:10:10 -0500

    Add models.py contains class and all the functions necessary for the book library

---
+++ models.py
@@ -0,0 +1,52 @@
+import json
+
+
+class Books:
+    def __init__(self):
+        try:
+            with open("books.json", "r") as f:
+                self.books = json.load(f)
+        except FileNotFoundError:
+            self.books = []
+
+    def all(self):
+        return self.books
+
+    def get(self, id):
+        book = [book for book in self.all() if book['id'] == id]
+        if book:
+            return book[0]
+        return []
+
+    def create(self, data):
+        self.books.append(data)
+        self.save_all()
+
+    def save_all(self):
+        with open("books.json", "w") as f:
+            json.dump(self.books, f)
+
+    def update(self, id, data):
+        book = self.get(id)
+        if book:
+            index = self.books.index(book)
+            self.books[index] = data
+            self.save_all()
+            return True
+        return False
+
+    def sort(self):
+        
+        sor_books = sorted(self.books, key=lambda book:book["title"])
+        return sor_books
+
+
+    def delete(self, id):
+        book = self.get(id)
+        if book:
+            self.books.remove(book)
+            self.save_all()
+            return True
+        return False
+ 
+books = Books()
